72c7ed3105177ef86ad72f60d93db880f52ebe83
emericriv <e19riviere@gmail.com>
2025-03-02 15:25:43 +0100

    Blank project again

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import "./App.css";
 import "./index.css";
-import { LoadingScreen } from "./components/LoadingScreen";
-import { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
-  return (
-    <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div
-        className={`min-h-screen transition-opacity duration-700 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
-      >
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
